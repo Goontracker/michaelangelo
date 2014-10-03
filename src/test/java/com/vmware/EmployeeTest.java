da5bdcbb1e57d369fd23a024b6dd8bfc25e3c540
Vinit Adkar <vinit.adkar@gmail.com>
2014-10-03 11:55:06 -0700

    Added pattern support for Employee

--- src/test/java/com/vmware/EmployeeTest.java
+++ src/test/java/com/vmware/EmployeeTest.java
@@ -2,13 +2,23 @@ package com.vmware;
 
 import static org.testng.Assert.*;
 
+import java.util.regex.Pattern;
+
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class EmployeeTest {
 
+	private Employee employee;
+	
+	@BeforeMethod
+	public void setUp() {
+		System.out.println("Construct a new Employee per Test");
+		employee = new Employee(Pattern.compile("\\d{3}-\\d{2}-\\d{4}"));
+	}
+	
 	@Test
 	public void createEmployeeAndGetName() {
-		Employee employee = new Employee();
 		String name = "Ricardo Montalban";
 		employee.setName(name);
 		assertEquals(employee.getName(), name);
@@ -19,14 +29,12 @@ public class EmployeeTest {
 	//CTRL+E Recent Files
 	@Test
 	public void addASocialSecurityNumber() {
-		Employee employee = new Employee();
 		employee.setSocialSecurityNumber("123-45-6789");
 		assertEquals(employee.getSocialSecurityNumber(), "123-45-6789");
 	}
 	
 	@Test
 	public void addAnotherSocialSecurityNumber() {
-		Employee employee = new Employee();
 		employee.setSocialSecurityNumber("123-45-6710");
 		assertEquals(employee.getSocialSecurityNumber(), "123-45-6710");
 	}
@@ -34,7 +42,6 @@ public class EmployeeTest {
 	@Test
 	public void fixDE30201_SocialSecurityMustBeACertainFormat() {
 		String badSSN = "sfsFSf";
-		Employee employee = new Employee();
 		try {
 			employee.setSocialSecurityNumber(badSSN);
 			fail("This line should not have reached");			
